50d8e59d2c172d52bce73c5fe4b052cfe4f8be72
Ryan Page <ryanapage@gmail.com>
2021-12-30 20:02:39 -0800

    add push option

--- bot/buildDocker.sh
+++ bot/buildDocker.sh
@@ -2,5 +2,10 @@
 
 npx tsc
 
-docker build . -t ${1:-ryanpage/guac-bot}:${2:-latest}
+DOCKER_TAG=${1:-ryanpage/guac-bot}:${2:-latest}
 
+docker build . -t $DOCKER_TAG
+
+if [[ ! -z "${PUSH}" ]]; then
+    docker push $DOCKER_TAG
+fi
