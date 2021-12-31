de7ba288047d0cc668b39305e328441cba1193a1
Ryan Page <ryanapage@gmail.com>
2021-12-31 12:37:18 -0800

    add new test case to confirm user init logic

--- bot/src/commands/bing.test.ts
+++ bot/src/commands/bing.test.ts
@@ -1,5 +1,6 @@
 import { createTestInteraction, getApiResult } from '../util/test-helper';
 import onCommandReceived from '../event/interaction-create';
+import prisma from '../util/prisma';
 
 it('should reply with bong when called', async () => {
     const int = createTestInteraction('bing');
@@ -8,3 +9,17 @@ it('should reply with bong when called', async () => {
     expect(spy).toHaveBeenCalled();
     expect(await getApiResult(spy)).toBe('bong');
 });
+
+it('should initialize the user to the database', async () => {
+    const int = createTestInteraction('bing');
+    const spy = jest.spyOn(int, 'reply');
+    await onCommandReceived(int, true);
+    expect(spy).toHaveBeenCalled();
+    expect(await getApiResult(spy)).toBe('bong');
+    const user = await prisma.user.findUnique({
+        where: {
+            discordId: int.user.id
+        }
+    });
+    expect(user).not.toBeNull();
+})
