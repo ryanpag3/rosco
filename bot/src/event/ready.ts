0d1c8f45dfe8ab8f523a656fb734f60ad106833a
Ryan Page <ryanapage@gmail.com>
2022-01-03 13:47:16 -0800

    set bot activity on ready

--- bot/src/event/ready.ts
+++ bot/src/event/ready.ts
@@ -1,5 +1,9 @@
+import client from '..';
 import logger from '../util/logger';
+import packageJson from '../../package.json';
 
 export default async function () {
-    logger.info(`🥑🥑🥑 Guac Bot is online and ready for action! 🥑🥑🥑`)
+    logger.info(`🥑🥑🥑 Guac Bot is online and ready for action! 🥑🥑🥑`);
+
+    client.user?.setActivity(`v${packageJson.version}`);
 }
--- bot/src/setup.ts
+++ bot/src/setup.ts
@@ -5,13 +5,17 @@ import './util/command-subcommand-map';
 import * as CommandDeployer from './util/slash-command-deployer';
 import onReady from './event/ready';
 import onInteractionCreate from './event/interaction-create';
+import packageJson from '../package.json';
 
 export default async function (client: Client) {
     // deploy slash commands
     await CommandDeployer.deploy();
 
+   
+
     // setup events
     client.on('ready', () => onReady());
     // @ts-ignore
     client.on('interactionCreate', async interaction => onInteractionCreate(interaction));
+
 }
