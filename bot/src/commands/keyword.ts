705267460ab17b00595cad285654bfaac53d4e79
Ryan Page <ryanapage@gmail.com>
2022-01-03 20:19:28 -0800

    skeleton of keyword commands

--- bot/src/commands/keyword.ts
+++ bot/src/commands/keyword.ts
@@ -9,7 +9,7 @@ const Keyword: Command = {
     options: [
         {
             name: 'create',
-            description: 'Create a keyword and assign it to a score.',
+            description: 'Create a keyword and assign it to a score. When users type the keyword the score will change.',
             type: ApplicationCommandOptionType.Subcommand,
             options: [
                 {
@@ -31,13 +31,54 @@ const Keyword: Command = {
                 },
                 {
                     name: 'amount',
-                    description: 'The amount to increase or decrease the score when keyword is found.',
+                    description: 'Optionally set the amount to increase or decrease the score when keyword is found.',
                     type: ApplicationCommandOptionType.Integer
                 },
                 {
                     name: 'channel',
                     description: 'Optionally limit the keyword detection to a specific channel.',
                     type: ApplicationCommandOptionType.Channel
+                },
+                {
+                    name: 'user',
+                    description: 'Optionally limit the keyword detection to a specific user.',
+                    type: ApplicationCommandOptionType.User
+                }
+            ]
+        },
+        {
+            name: 'delete',
+            description: 'Delete a keyword for a score.',
+            type: ApplicationCommandOptionType.Subcommand,
+            options: [
+                {
+                    name: 'keyword',
+                    description: 'The keyword or phrase you want to delete.',
+                    type: ApplicationCommandOptionType.String,
+                    required: true
+                },
+                {
+                    name: 'score-name',
+                    description: 'The name of the score that the keyword is assigned to.',
+                    type: ApplicationCommandOptionType.String,
+                    required: true
+                }
+            ]
+        },
+        {
+            name: 'list',
+            description: 'List out active keywords.',
+            type: ApplicationCommandOptionType.Subcommand,
+            options: [
+                {
+                    name: 'keyword',
+                    description: 'Optionally, filter the list by a keyword or phrase.',
+                    type: ApplicationCommandOptionType.String
+                },
+                {
+                    name: 'score-name',
+                    description: 'Optionally, filter the list by keywords assigned to this score.',
+                    type: ApplicationCommandOptionType.String
                 }
             ]
         }
