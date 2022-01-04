fede8b895577e35195f29a0a8bfcf061bfe9d7a8
Ryan Page <ryanapage@gmail.com>
2022-01-03 16:47:44 -0800

    add test case for permission list

---
+++ bot/src/commands/permission-list.sub.test.ts
@@ -0,0 +1,25 @@
+import * as ScoreService from '../service/score';
+import { createTestInteraction } from '../util/test-helper'
+import onCommandReceived from '../event/interaction-create';
+
+it('should list out the scores', async () => {
+    let int = createTestInteraction('permission', 'set-all', {
+        role: {
+            id: '1',
+            name: 'test'
+        }
+    });
+
+    await onCommandReceived(int);
+
+    int = createTestInteraction('permission', 'list', {}, '1');
+
+    const spy = jest.spyOn(int, 'reply');
+    
+    await onCommandReceived(int);
+
+    expect(spy).toHaveBeenCalled();
+
+    // @ts-ignore
+    expect(spy.mock.calls[0][0].embeds.length).toBe(1);
+});
