29e876320d5ac31ce9898d829513c71d2f05fb1a
Ryan Page <ryanapage@gmail.com>
2022-01-04 10:15:07 -0800

    two more test cases for create

--- bot/src/commands/keyword-create.sub.test.ts
+++ bot/src/commands/keyword-create.sub.test.ts
@@ -24,3 +24,30 @@ it('should create the keyword', async () => {
 
     expect(keywords.length).toBe(1);
 });
+
+it('should thrown an error if the score does not exist', async () => {
+    const int = createTestInteraction('keyword', 'create', {
+        'score-name': 'test',
+        keyword: 'iwinagainlewstherin'
+    }, '1');
+
+    await expect(onCommandReceived(int)).rejects.toThrow();
+});
+
+it('should throw an error with an invalid action', async () => {
+    let int = createTestInteraction('score', 'create', {
+        name: 'test',
+        description: 'description',
+        amount: 1
+    }, '1');
+    
+    await onCommandReceived(int);
+
+    int = createTestInteraction('keyword', 'create', {
+        'score-name': 'test',
+        keyword: 'iwinagainlewstherin',
+        action: 'whoa'
+    }, '1');
+
+    await expect(onCommandReceived(int)).rejects.toThrow();
+});
