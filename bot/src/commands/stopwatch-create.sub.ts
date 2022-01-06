bd48cd7de83a89ec218d086d9b6f77be13cde205
Ryan Page <ryanapage@gmail.com>
2022-01-05 22:48:48 -0800

    add stopwatch schema

---
+++ bot/src/commands/stopwatch-create.sub.ts
@@ -0,0 +1,11 @@
+import { Command } from '../../types/command';
+
+const Stopwatch: Command  = {
+    id: '00923569-fbc4-4ccf-a8ba-c625eaa5cbea',
+    name: 'stopwatch create',
+    handler: async (interaction, user) => {
+        // TODO
+    }
+}
+
+export default Stopwatch;
---
+++ bot/src/commands/stopwatch.ts
@@ -0,0 +1,39 @@
+import { ApplicationCommandOptionType } from 'discord-api-types';
+import { ApplicationCommandOptionTypes } from 'discord.js/typings/enums';
+import { Command } from '../../types/command';
+
+const Stopwatch: Command = {
+    id: '93181fbc-4e4b-478e-90a6-3e0985046ac8',
+    name: 'stopwatch',
+    description: `Perform various stopwatch commands.`,
+    options: [
+        {
+            name: `create`,
+            description: `Create and optionally start a stopwatch.`,
+            type: ApplicationCommandOptionType.Subcommand,
+            options: [
+                {
+                    name: `name`,
+                    description: `The unique name of the stopwatch.`,
+                    type: ApplicationCommandOptionType.String,
+                    required: true
+                },
+                {
+                    name: `start-on-created`,
+                    description: `Start the stopwatch when created.`,
+                    type: ApplicationCommandOptionType.Boolean
+                }
+            ]
+        }
+    ],
+    handler: async (interaction, user) => {
+        const subcommand = interaction.options.getSubcommand();
+
+        switch(subcommand){
+            case 'create':
+                return  
+        }
+    }
+}
+
+export default Stopwatch;
