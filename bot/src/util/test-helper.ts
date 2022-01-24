25c3c213c4ee5fd35f4659aa626fcec8613cfbab
Ryan Page <ryanapage@gmail.com>
2022-01-23 20:58:49 -0800

    fix test cases

--- bot/src/util/test-helper.ts
+++ bot/src/util/test-helper.ts
@@ -24,6 +24,7 @@ export function createTestInteraction(commandName: string, subcommandName?: stri
             } as any
         },
         isCommand: () => true,
+        isButton: () => false,
         // @ts-ignore
         toJSON: () => {},
         channel: {
