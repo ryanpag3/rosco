b9b6b50b0a7e57714d5132b0323b93f3534d2175
Ryan Page <ryanapage@gmail.com>
2022-01-25 08:12:00 -0800

    add test cases for poll info

---
+++ bot/src/commands/poll-info.sub.test.ts
@@ -0,0 +1,42 @@
+import onInteractionCreate from '../event/interaction-create';
+import prisma from '../util/prisma';
+import { createTestInteraction } from '../util/test-helper';
+
+it('should get info on a poll', async () => {
+    const name = 't';
+    let int = createTestInteraction('poll', 'create', {
+        name,
+        question: 'q',
+        'option-1': '1',
+        'option-2': '2'
+    });
+
+    await onInteractionCreate(int);
+
+    const poll = await prisma.poll.findUnique({
+        where: {
+            name: 't'
+        }
+    });
+
+    expect(poll).not.toBeNull();
+
+    int = createTestInteraction('poll', 'info', {
+        name
+    })
+
+    const spy = jest.spyOn(int, 'reply');
+
+    await onInteractionCreate(int);
+
+    // @ts-ignore
+    expect(spy.mock.calls[0][0].embeds.length).toBe(1);
+});
+
+it('should throw an error if a poll does not exist', async () => {
+    const int = createTestInteraction('poll', 'info', {
+        name: 'waht'
+    })
+
+    await expect(onInteractionCreate(int)).rejects.toThrow();
+});
