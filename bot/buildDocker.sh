aa36c9ac195fcc83a4ef6b5abd2765b9c330460a
Ryan Page <ryanapage@gmail.com>
2022-02-04 15:58:47 -0800

    fix docker build

--- bot/buildDocker.sh
+++ bot/buildDocker.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
-npx tsc
+yarn migrate deploy
+
+rm -rf ./out
+
+npx tsc 
+
+cd out && \
+ rm -rf **/*.min.js && \
+ npx uglifyjs-folder . -x .js -eo minified && \
+ cd -
 
 DOCKER_TAG=${1:-ryanpage/guac-bot}:${2:-latest}
+
+
+pwd
+
 
 docker build . -t $DOCKER_TAG
 
--- bot/src/util/prisma.ts
+++ bot/src/util/prisma.ts
@@ -1,13 +1,16 @@
 import { PrismaClient } from '@prisma/client';
 import path from 'path';
-import { sortPrismaSchema } from 'prisma-schema-sorter';
 import logger from './logger';
 
 const prisma = new PrismaClient({
     rejectOnNotFound: true
 });
 
-sortPrismaSchema(path.join(__dirname, '../../prisma/schema.prisma'))
-    .then(() => logger.debug('prisma schema sorted'));
+if (process.env.NODE_ENV !== 'production') {
+    const { sortPrismaSchema } = require('prisma-schema-sorter');
+
+    sortPrismaSchema(path.join(__dirname, '../../prisma/schema.prisma'))
+        .then(() => logger.debug('prisma schema sorted'));
+}
 
 export default prisma;
