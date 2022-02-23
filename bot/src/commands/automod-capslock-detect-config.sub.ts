fe3391fab9bd0e21203cd6e06344427f95ab9239
Ryan Page <ryanapage@gmail.com>
2022-02-23 15:12:59 -0800

    capslock detection config w/ test cases

--- bot/src/commands/automod-capslock-detect-config.sub.ts
+++ bot/src/commands/automod-capslock-detect-config.sub.ts
@@ -1,10 +1,34 @@
 import { Command } from '../../types/command';
+import prisma from '../util/prisma';
 
 const CapslockDetectConfig: Command = {
     id: 'cd3d208e-817d-41a1-a06c-54b9768b063d',
     name: 'capslock-detect config',
     handler: async (interaction, user, server) => {
+        const length = interaction.options.getInteger('length') || 12;
 
+        await prisma.server.update({
+            where: {
+                id: server.id
+            },
+            data: {
+               autoModCapslockDetectLength: length 
+            }
+        });
+
+        return interaction.reply({
+            embeds: [
+                {
+                    title: `:gear: Capslock Detection Configuration Updated`,
+                    fields: [
+                        {
+                            name: 'length',
+                            value: length?.toString() || server.autoModCapslockDetectLength.toString()
+                        }
+                    ]
+                }
+            ]
+        })
     }
 };
 
