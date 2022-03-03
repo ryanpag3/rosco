44ca47e75d5ba1e8d92f28c4e500564cf79d0122
Ryan Page <ryanapage@gmail.com>
2022-03-03 08:16:45 -0800

    refactor keyword create to handle errors better

--- bot/src/commands/keyword/keyword-create.sub.ts
+++ bot/src/commands/keyword/keyword-create.sub.ts
@@ -1,8 +1,10 @@
 import { KeywordAction, Prisma } from '@prisma/client';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 import { Command } from '../../../types/command';
 import KeywordCache from '../../service/keyword-cache';
 import BotError from '../../util/bot-error';
 import prisma from '../../util/prisma';
+import PrismaErrorCode from '../../util/prisma-error-code';
 
 const KeywordCreate: Command = {
     id: '6ec5e902-f482-48a5-879e-7427b8ba5a20',
@@ -15,36 +17,43 @@ const KeywordCreate: Command = {
         const channel = interaction.options.getChannel('channel');
         const filterOnUser = interaction.options.getUser('user');
 
-        const score = await prisma.score.findUnique({
-            where: {
-                name_serverId: {
-                    name: scoreName,
-                    serverId: server?.id as string
-                }
-            }
-        });
-
-        if (!score)
-            throw new BotError(`Could not find score to assign keyword to.`);
-
         if (action && action !== 'UP' && action !== 'DOWN')
             throw new BotError(`Invalid action provided. Only \`UP\` or \`DOWN\` are allowed.`);
 
-        let inDbUser;
-
-        if (filterOnUser) {
-            inDbUser = await prisma.user.findUnique({
+        let score;
+        try {
+            score = await prisma.score.findUnique({
                 where: {
-                    discordId: user.id
+                    name_serverId: {
+                        name: scoreName,
+                        serverId: server?.id as string
+                    }
                 }
             });
+        } catch (e) {
+            if ((e as PrismaClientKnownRequestError).code === PrismaErrorCode.NOT_FOUND)
+                throw new BotError('Could not find score to assign keyword to.');
+        }
+
+        let inDbUser;
+        try {
+            if (filterOnUser) {
+                inDbUser = await prisma.user.findUnique({
+                    where: {
+                        discordId: user.id
+                    }
+                });
+            }
+        } catch (e) {
+            if ((e as PrismaClientKnownRequestError).code === PrismaErrorCode.NOT_FOUND)
+                throw new BotError('Could not find user to filter on.');
         }
 
         try {
             const keywordRecord = await prisma.keyword.create({
                 data: {
                     word,
-                    scoreId: score.id,
+                    scoreId: score?.id as string,
                     serverId: server?.id as string,
                     channelId: channel?.id,
                     amount: amount || undefined,
@@ -64,16 +73,10 @@ const KeywordCreate: Command = {
                 ]
             })
         } catch (e) {
-            if (!(e instanceof Prisma.PrismaClientKnownRequestError)) {
-                throw e;
-            } else {
-                if (e.code === 'P2002') {
-                    throw new BotError(`A keyword already exists with that configuration.`);
-                }
-                throw e;
-            }
+            if ((e as PrismaClientKnownRequestError).code === PrismaErrorCode.UNIQUE_COHSTRAINT)
+                throw new BotError(`A keyword already exists with that configuration.`);
+            throw e;
         }
-
     }
 };
 
