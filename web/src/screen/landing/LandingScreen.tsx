86f93d494bb6a2624c6d411b687ff607dd0ffe8b
Ryan Page <ryanapage@gmail.com>
2022-03-19 09:55:11 -0700

    add stage header text

--- web/src/screen/landing/LandingScreen.tsx
+++ web/src/screen/landing/LandingScreen.tsx
@@ -6,20 +6,33 @@ import AppName from './AppName'
 import Column from 'component/Column'
 import InviteMenu from './InviteMenu'
 import LearnMore from './LearnMore'
+import Row from 'component/Row'
 
 const LandingScreen = () => {
   return (
     <Screen>
-        <Header/>
-        <ContentColumn>
-          <StyledAppName/>
-          <InviteMenu/>
-        </ContentColumn>
-        <LearnMore/>
-        {/* {"test\n".repeat(10000)} */}
+        <Stage>
+          <Header/>
+          <ContentColumn>
+            <StyledAppName/>
+            <InviteMenu/>
+          </ContentColumn>
+          <LearnMore/>
+          {/* {"test\n".repeat(10000)} */}
+        </Stage>
+        <Stage>
+          <StyledRow>
+            <StageHeaderText>ROSCO is jam-packed with several modules to help manage, moderate, and engage your community.</StageHeaderText>
+          </StyledRow>
+        </Stage>
     </Screen>
   )
 }
+
+const Stage = styled.div`
+  height: 100vh;
+  width: 100%;
+`;
 
 const ContentColumn = styled(Column)`
   width: 100%;
@@ -28,6 +41,20 @@ const ContentColumn = styled(Column)`
 
 const StyledAppName = styled(AppName)`
 
+`;
+
+const StyledRow = styled(Row)`
+  max-width: 90%;
+`;
+
+const StageHeaderText = styled.h2`
+  font-weight: normal;
+  font-size: 3.75em;
+  padding-left: .3em;
+`;
+
+const StageHeaderTextEmphasized = styled(StageHeaderText)`
+  font-weight: bold;
 `;
 
 export default LandingScreen
