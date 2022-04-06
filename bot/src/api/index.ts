ea6f4ebc2e53615a85b69a3e25b838178ac2be34
Ryan Page <ryanapage@gmail.com>
2022-04-06 07:53:58 -0700

    begin dashboard implementation

--- bot/src/api/index.ts
+++ bot/src/api/index.ts
@@ -1,4 +1,6 @@
-require('dotenv').config();
+require('dotenv').config({
+    path: '../../.env'
+});
 
 import Fastify from 'fastify';
 import logger from '../util/logger';
--- bot/test-setup.ts
+++ bot/test-setup.ts
@@ -1,56 +1,9 @@
 require('dotenv').config({
     path: '.env.test'
 });
-import execa from 'execa';
-import logger from './src/util/logger';
-import prisma from './src/util/prisma';
-// import redis from './src/util/redis';
 import redis from 'redis-mock';
 
 jest.mock('./src/util/prisma.ts');
 jest.mock('redis', () => redis);
-
-
-// beforeAll(async () => {
-    
-//     let stdout = await execa.command('yarn migrate reset --force');
-//     logger.trace(stdout);
-
-//     stdout = await execa.command('yarn migrate deploy');
-//     logger.trace(stdout);
-
-//     try {
-//         await redis.connect();
-//     } catch (e) {
-//         // noop
-//     }
-// }, 30000);
-
-beforeEach(async () => {
-    // //@ts-ignore
-    // for (const { tablename } of await prisma.$queryRaw`SELECT tablename FROM pg_tables WHERE schemaname='public'`) {
-    //     if (tablename !== '_prisma_migrations' && !tablename.startsWith('_')) {
-    //         // @ts-ignore
-    //         await prisma[camelize(tablename)].deleteMany({where: {}});
-    //         logger.trace(`truncated ${tablename}`);
-    //     } 
-    // }
-
-    // try {
-    //     await redis.connect();
-    // } catch (e) {
-    //     // noop
-    // }
-});
-
-// afterEach(async () => {
-//     await prisma.$disconnect();
-// });
-
-function camelize(str: string) {
-    return str.replace(/(?:^\w|[A-Z]|\b\w)/g, function(word, index) {
-      return index === 0 ? word.toLowerCase() : word.toUpperCase();
-    }).replace(/\s+/g, '');
-}
 
 export default {}
