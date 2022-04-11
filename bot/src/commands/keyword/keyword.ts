8cb7554e598756282044ee48369dfb829fdf45ea
Ryan Page <ryanapage@gmail.com>
2022-04-10 19:01:54 -0700

    update the keyword command definitions to be accurate

--- bot/src/commands/keyword/keyword.ts
+++ bot/src/commands/keyword/keyword.ts
@@ -62,15 +62,35 @@ const Keyword: Command = {
             options: [
                 {
                     name: 'keyword',
-                    description: 'The keyword or phrase you want to delete.',
+                    description: 'The keyword or phrase to check.',
                     type: ApplicationCommandOptionType.String,
                     required: true
                 },
                 {
                     name: 'score-name',
-                    description: 'The name of the score that the keyword is assigned to.',
+                    description: 'The name of the score to assign the keyword to.',
                     type: ApplicationCommandOptionType.String,
                     required: true
+                },
+                {
+                    name: 'action',
+                    description: 'Which action was defined for the keyword you are deleting.',
+                    type: ApplicationCommandOptionType.String
+                },
+                {
+                    name: 'channel',
+                    description: 'Which channel the keyword is assigned to. (Optional)',
+                    type: ApplicationCommandOptionType.Channel
+                },
+                {
+                    name: 'user',
+                    description: 'Which user the keyword is assigned to. (Optional)',
+                    type: ApplicationCommandOptionType.User
+                },
+                {
+                    name: 'role',
+                    description: 'Which role the keyword is assigned to. (Optional)',
+                    type: ApplicationCommandOptionType.Role
                 }
             ]
         },
