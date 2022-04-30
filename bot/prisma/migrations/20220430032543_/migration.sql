7039f961f24670740cff520c64a129b9f966d99a
Ryan Page <ryanapage@gmail.com>
2022-04-29 20:27:40 -0700

    fix issue with database migration

--- bot/prisma/migrations/20220430032543_/migration.sql
+++ bot/prisma/migrations/20220430032543_/migration.sql
@@ -6,7 +6,8 @@
 
 */
 -- AlterTable
-ALTER TABLE "Keyword" ADD COLUMN     "name" TEXT NOT NULL;
+ALTER TABLE "Keyword" ADD COLUMN     "announceChannelId" TEXT,
+ADD COLUMN     "name" TEXT NOT NULL DEFAULT gen_random_uuid();
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Keyword_name_key" ON "Keyword"("name");
