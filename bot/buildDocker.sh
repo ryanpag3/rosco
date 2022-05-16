4215b82a598e186e9eb19a02faaaf05ff6b5d535
Ryan Page <ryanapage@gmail.com>
2022-05-16 15:56:24 -0700

    add logs to docker build

--- bot/buildDocker.sh
+++ bot/buildDocker.sh
@@ -2,10 +2,13 @@
 
 rm -rf ./out
 
+echo "compiling project"
 npx tsc 
 
+echo "generating schema"
 npx prisma generate
 
+echo "minifying"
 cd out && \
  rm -rf **/*.min.js && \
  npx uglifyjs-folder . -x .js -eo minified && \
@@ -13,10 +16,9 @@ cd out && \
 
 DOCKER_TAG=${1:-ryanpage/rosco}:${2:-latest}
 
-
 pwd
 
-
+echo "building image"
 docker build . -t $DOCKER_TAG --progress=plain
 
 if [[ ! -z "${PUSH}" ]]; then
