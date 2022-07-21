6d79642c2fac4b770335b3a9fd3da2b9b56510ab
Ryan Page <ryanapage@gmail.com>
2022-07-21 16:24:57 -0700

    add (extremely basic) home screen

--- web/src/screen/home/index.tsx
+++ web/src/screen/home/index.tsx
@@ -4,14 +4,29 @@ import styled from 'styled-components'
 
 const Home = () => {
   return (
-      <StyledScreen>
-          Home
-      </StyledScreen>
+    <StyledScreen>
+      <WelcomeText>
+        Welcome to the Rosco Web Dashboard!
+        <br /><br />
+      </WelcomeText>
+      <SubText>
+        Please use the navigation menu to the left to get started.
+      </SubText>
+    </StyledScreen>
   )
 }
 
 const StyledScreen = styled(Screen)`
+  padding: 1em;
+`;
 
+const WelcomeText = styled.span`
+  font-size: 2em;
+  max-width: 25em;
+`;
+
+const SubText = styled.span`
+  font-size: 1.5em;
 `;
 
 export default Home
