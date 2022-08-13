087179a04408da0b020a29591a03558758af28c0
Ryan Page <ryanapage@gmail.com>
2022-08-13 10:18:33 -0700

    slight adjustments to automod functionality

--- web/src/component/ScreenTopBar/ServerSelect.tsx
+++ web/src/component/ScreenTopBar/ServerSelect.tsx
@@ -38,6 +38,12 @@ const ServerSelect = () => {
                 icon: s.icon,
                 id: s.id
             }
+        }).sort((a: any, b: any) => {
+            if (a.value > b.value)
+                return 1;
+            if (a.value < b.value)
+                return -1;
+            return 0;
         })
         return servers;
     }
